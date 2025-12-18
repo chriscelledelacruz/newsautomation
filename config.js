7aeceb3ae3a1d2ca090391f28bdb00766d7c97ac
ChrissyDLC <chriscelle.delacruz@staffbase.com>
2025-12-17 21:35:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-    API_TOKEN: 'paste-your-real-token-here',
-    INSTALLATION_ID: 'paste-your-real-id-here',
+    API_TOKEN: 'Njk0MzY2NWRjZDJlNTQ3MjAwYmI5YWJkOkZmX2pxVGxBZDJjKzFeW0s3LmhLMWFiXzBpNlohJG4qJjRWdGl+cXYmZWs0ME05LmkmIS02YkgtWlpYLDVSWGk=',
+    INSTALLATION_ID: '68c9900550b265407de670e5',
     BASE_URL: 'https://app.staffbase.com/api'
 };
